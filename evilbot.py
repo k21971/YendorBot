f1f2cdc41e587071e5be9cfe4ea0694229a92ea6
k21971 <keith.simpson1971@gmail.com>
2021-05-21 09:46:19 -0400

    Set umode -R in order to receive Pinoclone messages.
    Thanks aoei.

--- evilbot.py
+++ evilbot.py
@@ -273,6 +273,7 @@ class DeathBotProtocol(irc.IRCClient):
     def signedOn(self):
         self.factory.resetDelay()
         self.startHeartbeat()
+        self.sendLine('MODE {} -R'.format(self.nickname))
         if not SLAVE: self.join(CHANNEL)
         random.seed()
 
